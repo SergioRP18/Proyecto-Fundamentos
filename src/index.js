9a25908fd7d7937048ecf6bcfec407a2f008808c
SergioRP18 <sergio.restrepo@u.icesi.edu.co>
2024-05-01 18:20:05 -0500

    arregla el constructor y crea las cards de forma dinamica

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 const listOfProducts = [
-    new Products("ULTRA MEGA PRO 999 RUNNING TREADMILL", "BhFitness", "../assets/shop.png", "../assets/650_1200.webp"),
-    new Products("Xcalibur EMS Smart Bike", "TechnoGym", "../assets/shop.png", "../assets/6652520389662785.webp"),
-    new Products("TF30 FOLDING TAPE WITH XIR CONSOLE", "MATRIX", "../assets/shop.png", "../assets/bicicleta-fija.jpg"),
-    new Products("Xcalibur Magnétic Smart Bike", "BhFitness", "../assets/shop.png", "../assets/Multigym-500-Behumax.jpg"),
-    new Products("PLATINUM CLUB SERIES TREADMILL", "Tunturi", "../assets/shop.png", "../assets/E5006-PRESION-HOMBRO.png"),
-    new Products("ACTIVATE SERIES LIFECYCLE VERTICAL EXERCISE BIKE", "MATRIX", "../assets/shop.png", "../assets/optimized---Go.png"),
+    
 ]
 
 function createAllProducts(){
     const results = shopData.results
-    for(let i; i < results.length; i++){
+    for(let i = 0; i < results.length; i++){
         const obj = results[i]
-        const price = `${obj.price}`
+        const favorite = obj.favorite
         const name = obj.title
         const brand = obj.brand
-        const imageUrl = obj.images
-        const product = new Products(name, brand,)
-
-
+        const imageUrl = obj.imageUrl;
+        const product = new Products(name, brand, favorite, imageUrl)
+        listOfProducts.push(product)
     }
 }
 
@@ -29,5 +23,7 @@ function fillScreenWithProducts(){
     }
 }
 
+
+createAllProducts()
 fillScreenWithProducts()
 
